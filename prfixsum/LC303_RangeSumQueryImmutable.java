c27db8d9dca2dced83495c44aaead1513c578b37
JDpatel69 <jenishdudhatrajenish63811@gmail.com>
2026-01-15 09:55:44 +0530

    Add LC 303 Range Sum Query (Prefix Sum, O(1) query)

---
+++ prfixsum/LC303_RangeSumQueryImmutable.java
@@ -0,0 +1,28 @@
+class NumArray {
+        int[] prefix;
+
+    public NumArray(int[] nums) {
+        prefix = new int[nums.length];
+        prefix[0]=nums[0];
+        for(int i=1;i<nums.length;i++){
+            prefix[i]=nums[i]+prefix[i-1];
+        }
+
+    }
+    
+    public int sumRange(int left, int right) {
+        int sum=0;
+        if(left==0) sum = prefix[right];
+        else
+        sum =prefix[right]-prefix[left-1];
+        return sum;
+        
+    }
+
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(left,right);
+ */
