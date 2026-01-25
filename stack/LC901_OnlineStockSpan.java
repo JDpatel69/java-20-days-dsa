8fcbb545797165c403a1e4942a478111ec250db6
JDpatel69 <jenishdudhatrajenish63811@gmail.com>
2026-01-25 15:37:48 +0530

    Add LC 901 Online Stock Span (Monotonic Stack)

---
+++ stack/LC901_OnlineStockSpan.java
@@ -0,0 +1,31 @@
+// LC 901 - Online Stock Span
+// Pattern: Monotonic Stack
+// Idea: Store {price, span}, pop while price >= stack top
+// Time Complexity: O(1) amortized
+// Space Complexity: O(n)
+class StockSpanner {
+
+    Stack<int []> st;
+
+    public StockSpanner() {
+        st = new Stack<>();
+    }
+
+    public int next(int price) {
+        int span = 1;
+
+        while (!st.isEmpty() && st.peek()[0] <= price) {
+            span += st.pop()[1];
+        }
+
+        st.push(new int[]{price, span});
+        return span;
+    }
+}
+
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
